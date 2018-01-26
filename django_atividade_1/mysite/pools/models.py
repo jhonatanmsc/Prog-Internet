85606c9dcaf3a6f6d565441513038fb9970111d1
Akijunior <suitsu19@gmail.com>
2018-01-25 23:30:29 -0200

    Correção no models e adição da ordenação por data.

--- django_atividade_1/mysite/pools/models.py
+++ django_atividade_1/mysite/pools/models.py
@@ -1,27 +1,32 @@
 from django.db import models
 
-class Question(models.Model):
-	question_text = models.TextField()
-	closed = models.BooleanField(default=False)
-	pub_date = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		mod = ('------------------------------\n'+
-				str(self.question_text)+'\n'+
-				'data de criação: '+str(self.pub_date)+'\n'+
-				'status: '+str(self.closed)+'\n'
-			)
-		return mod
+class Question(models.Model):
+    question_text = models.TextField()
+    closed = models.BooleanField(default=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        mod = ('------------------------------\n' +
+               str(self.question_text) + '\n' +
+               'data de criação: ' + str(self.pub_date) + '\n' +
+               'status: ' + str(self.closed) + '\n'
+               )
+        return mod
+
+    class Meta:
+        ordering = ['-pub_date']
+
 
 class Choice(models.Model):
-	question = models.ManyToManyField(Question, null=True)
-	choice_text = models.TextField()
-	votes = models.IntegerField(default=0)
+    question = models.ForeignKey('Question', null=True, related_name='alternativas_associadas', on_delete=models.CASCADE)
+    choice_text = models.TextField()
+    votes = models.IntegerField(default=0)
 
-	def __str__(self):
-		mod = (
-			self.question+'\n'+
-			'resposta: '+str(self.choice_text)+'\n'+
-			'likes: '+str(self.votes)+'\n'
-		)
-		return mod
+    def __str__(self):
+        mod = (
+            self.question + '\n' +
+            'resposta: ' + str(self.choice_text) + '\n' +
+            'likes: ' + str(self.votes) + '\n'
+        )
+        return mod
--- django_atividade_1/mysite/pools/urls.py
+++ django_atividade_1/mysite/pools/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 
 from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('^questao/<int:pk>', exibir_questao, name='exibir_questao'),
+    path('questao/<int:pk>', exibir_questao, name='exibir_questao'),
 ]
