689a24bea6c19003e1e91450955b16da0606fb60
Jhonatan Matheus <jhonatanmatheus80@gmail.com>
2018-06-30 00:03:40 -0300

    bit update

--- atividade_T6/perfil/serializers.py
+++ atividade_T6/perfil/serializers.py
@@ -9,3 +9,24 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'user_posts')
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('url', 'name', 'raca', 'characterType', 'native', 'musicTheme')
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Comment
+        fields = ('url', 'name', 'itemClass', 'effect', 'patter', 'location', 'cost')
+
+
+class MusicSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Address
+        fields = ('url', 'name')
